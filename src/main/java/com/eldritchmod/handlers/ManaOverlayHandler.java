a65e939e3b9d9ec280908096bbde49166abe8ccd
Thechiefpotatopeeler <69015883+Thechiefpotatopeeler@users.noreply.github.com>
2022-04-25 16:25:56 +1200

    The mana overlay was working, but I changed it.

--- src/main/java/com/eldritchmod/handlers/ManaOverlayHandler.java
+++ src/main/java/com/eldritchmod/handlers/ManaOverlayHandler.java
@@ -11,7 +11,7 @@ import com.eldritchmod.data.IMana;
 
 public class ManaOverlayHandler extends Gui {
 
-    private final ResourceLocation manaBar = new ResourceLocation("eldritch_mod", "textures/gui/mana_bar.png");
+    private final ResourceLocation manaBar = new ResourceLocation(Main.MOD_ID, "textures/GUI/manabar");
     private int tex_width = 5, tex_height = 100;
 
     @SubscribeEvent
@@ -19,9 +19,6 @@ public class ManaOverlayHandler extends Gui {
         if (event.getType() == RenderGameOverlayEvent.ElementType.TEXT){
             Minecraft mc = Minecraft.getMinecraft();
             mc.renderEngine.bindTexture(manaBar);
-            IMana mana = mc.player.getCapability(ManaProvider.MANA_CAP, null); //Gets the mana
-            //float barUnit = (float) bar_width / (float) IMana.MAXIMUM_MANA; //Calculates the width of the bar
-            //int currentWidth = (int) barUnit * mana.getMana();
             drawTexturedModalRect(0,0,0,0,tex_width,tex_height); //The first drawing of the bar
         }
     }
