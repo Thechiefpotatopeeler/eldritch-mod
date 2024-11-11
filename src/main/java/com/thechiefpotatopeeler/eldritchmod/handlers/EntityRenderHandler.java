4e03380d6219631298bfcbe7b51fbd14a03fcfb7
Thechiefpotatopeeler <69015883+Thechiefpotatopeeler@users.noreply.github.com>
2024-11-12 12:28:38 +1300

    Added render handling for new entities

--- src/main/java/com/thechiefpotatopeeler/eldritchmod/handlers/EntityRenderHandler.java
+++ src/main/java/com/thechiefpotatopeeler/eldritchmod/handlers/EntityRenderHandler.java
@@ -6,6 +6,7 @@ import com.thechiefpotatopeeler.eldritchmod.entity.entity.moobloom.EntityRedMoob
 import com.thechiefpotatopeeler.eldritchmod.entity.entity.moobloom.EntityYellowMoobloom;
 
 import com.thechiefpotatopeeler.eldritchmod.entity.entity.*;
+import com.thechiefpotatopeeler.eldritchmod.entity.entity.shroomoid.ShroomoidTrader;
 import com.thechiefpotatopeeler.eldritchmod.entity.render.*;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraft.client.renderer.entity.Render;
@@ -16,7 +17,6 @@ public class EntityRenderHandler {
 
     public static void registerEntityRenders() {
         RenderingRegistry.registerEntityRenderingHandler(EntitySpiderMatriarch.class, new IRenderFactory<EntitySpiderMatriarch>(){
-
             @Override
             public Render<? super EntitySpiderMatriarch> createRenderFor(RenderManager manager) {
                 return new RenderMatriarch(manager);
@@ -26,7 +26,6 @@ public class EntityRenderHandler {
         
             
         RenderingRegistry.registerEntityRenderingHandler(EntityYellowMoobloom.class,new IRenderFactory<EntityYellowMoobloom>(){
-
             @Override
             public Render<? super EntityYellowMoobloom> createRenderFor(RenderManager manager) {
                 return new RenderYellowMoobloom(manager);
@@ -34,7 +33,6 @@ public class EntityRenderHandler {
         });
         
         RenderingRegistry.registerEntityRenderingHandler(EntityRedMoobloom.class,new IRenderFactory<EntityRedMoobloom>(){
-
             @Override
             public Render<? super EntityRedMoobloom> createRenderFor(RenderManager manager) {
                 return new RenderRedMoobloom(manager);
@@ -42,7 +40,6 @@ public class EntityRenderHandler {
         });
 
         RenderingRegistry.registerEntityRenderingHandler(EntityPoobloom.class,new IRenderFactory<EntityPoobloom>(){
-                
                 @Override
                 public Render<? super EntityPoobloom> createRenderFor(RenderManager manager) {
                     return new RenderPoobloom(manager);
@@ -50,7 +47,6 @@ public class EntityRenderHandler {
             });
 
         RenderingRegistry.registerEntityRenderingHandler(EntityBarnacle.class,new IRenderFactory<EntityBarnacle>(){
-                
             @Override
             public Render<? super EntityBarnacle> createRenderFor(RenderManager manager) {
                 return new RenderBarnacle(manager);
@@ -58,7 +54,6 @@ public class EntityRenderHandler {
         });
 
         RenderingRegistry.registerEntityRenderingHandler(EntityDwarf.class,new IRenderFactory<EntityDwarf>(){
-
             @Override
             public Render<? super EntityDwarf> createRenderFor(RenderManager manager) {
                 return new RenderDwarf(manager);
@@ -100,6 +95,20 @@ public class EntityRenderHandler {
             @Override
             public Render<? super EntityScarecrow> createRenderFor(RenderManager manager) {
                 return new RenderScareCrow(manager);
+            }
+        });
+        RenderingRegistry.registerEntityRenderingHandler(ShroomoidTrader.class,new IRenderFactory<ShroomoidTrader>(){
+
+            @Override
+            public Render<? super ShroomoidTrader> createRenderFor(RenderManager manager) {
+                return new RenderShroomoid(manager);
+            }
+        });
+        RenderingRegistry.registerEntityRenderingHandler(EntityGoatman.class,new IRenderFactory<EntityGoatman>(){
+
+            @Override
+            public Render<? super EntityGoatman> createRenderFor(RenderManager manager) {
+                return new RenderGoatman(manager);
             }
         });
     }
